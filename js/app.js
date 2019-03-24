6deb9385702794b5856d51dae82a2a94af688f58
Jesse Dispoto <jester070993@gmail.com>
2019-03-23 19:35:25 -0700

    final, no search input dropdown feature yet

--- js/app.js
+++ js/app.js
@@ -4,8 +4,10 @@ let chart3 = document.getElementById("chart3").getContext("2d");
 
 //global chart options
 Chart.defaults.global.defaultFontSize = 14;
+//charts, graphs made up data
 
-//chart1
+//chart1 
+
 let data1 = new Chart(chart1, {
 	type: "line",
 	data: {
@@ -55,7 +57,7 @@ let data1 = new Chart(chart1, {
 			enabled: true
 		}
 	}
-})
+});
 
 //chart2
 
@@ -72,9 +74,9 @@ let  dailyTraffic = new Chart (chart2, {
 	options: {
 		responsive: true
 	}
-})
+});
 
-
+//chart 3
 let  deviceData = new Chart (chart3, {
 	type: "doughnut",
 	data: {
@@ -93,9 +95,170 @@ let  deviceData = new Chart (chart3, {
 		responsive: true,
 		title:{
 			display: true,
-			text: "Device type by percentage",
+			text: "User Access by Device Type",
 			fontSize: 16
 		},
 		cutoutPercentage: 45
 	}
+});
+
+//varibales not related to chart 
+
+const exit = document.getElementById("exit")
+const alertBox = document.querySelector(".alertBox")
+
+//removes alert box on dashboard when x cliked 
+//possibly animate? 
+exit.addEventListener("click", ()=> {
+	alertBox.style.display = "none";
+	})
+
+
+
+//click bell icon removes green notification dot
+const greenDot = document.querySelector(".greenDot");
+const bell = document.querySelector(".bell");
+const alertNotifications = document.querySelector(".alertNotifications");
+const alertCircle = document.querySelectorAll(".alertCircle");
+
+//notifications bell actions
+function toggleNotifications(items){
+	items.style.display = "block";
+}
+function removeNotifications(items){
+	items.style.display = "none";
+}
+
+//toggles notifciations, removes green indication
+bell.addEventListener("click", () => {
+	if(alertNotifications.style.display == "none"){
+		toggleNotifications(alertNotifications);		
+		greenDot.style.display = "none";
+	}
+	else {
+		removeNotifications(alertNotifications);
+	}
+});
+
+//removes a notification when the x  is clicked (add transition?)
+for (let i = 0 ; i < alertCircle.length ; i++) {
+	alertCircle[i].addEventListener("click", () =>{
+		alertCircle[i].parentNode.remove()
+	})
+}
+
+
+//simulate submitting message form, required fields, etc.
+const send  = document.querySelector(".send");
+const form  =  document.querySelector(".form");
+const successMessage =  document.querySelector(".successMessage")
+const requireMessage =  document.querySelector(".requireMessage")
+
+
+form.addEventListener("submit", (e) => {
+		const textarea = document.querySelector("textarea");
+		const forminput = document.querySelector(".forminput");
+
+		//if value has no length (nothing in input)
+		if (textarea.value.length == 0 || forminput.value.length == 0){
+				e.preventDefault();
+				//styles required input box
+				forminput.style.borderColor = "red";
+				textarea.style.borderColor = "red";
+				requireMessage.style.display = "block";
+
+				//adds required text keyframe
+				requireMessage.classList.add("slideMe");
+				//removes keyframe class after 1 second so animation happens again if button pressed again
+				setTimeout(() => {
+			      	requireMessage.classList.remove("slideMe");
+			    }, 1000);
+
+				//sets colors back to default after 1 second delay
+				setTimeout(()=>{
+				requireMessage.style.display = "none"
+				forminput.style.borderColor = "lightgrey";
+				textarea.style.borderColor = "lightgrey";
+			}, 1000);
+		} 
+		else {
+			// e.preventDefault();
+			//displays  sucess message, adds keyframe animate class to slide up
+			successMessage.style.display = "block";
+			successMessage.classList.add("slideMe");
+
+			//waits 2 seconds before removing the slide, to replay animation if needed
+			setTimeout(() => {
+			      	successMessage.classList.remove("slideMe");
+			    }, 2000);
+
+			//styles successs box 
+			requireMessage.style.display = "none"
+			forminput.style.borderColor = "limegreen";
+			textarea.style.borderColor = "limegreen";
+
+			//styles box back to normal after 1 second
+			setTimeout( ()=>{
+				successMessage.style.display = "none"
+				forminput.style.borderColor = "lightgrey";
+				textarea.style.borderColor = "lightgrey";
+			}, 1000);
+
+			//save input to local storage
+			localStorage.setItem(forminput.value, textarea.value);
+
+			forminput.value = "" ;
+			textarea.value = " ";
+
+		}		
 })
+
+//autocomplete
+//autocomplete function 
+//  const forminput = document.querySelector(".forminput")
+//  let  autocompleteResults = document.querySelector(".autocomplete-results");
+//  const nameList = ["Dawn Wood", "Dan Oliver" , "Dale Byrd" , "Victoria Chambers", "Mike Gordon",
+//  				"Jon Fishman", "Page McConell", "Trey Anastasio",  "Chris Kuroda", "Junior Munioz"];
+//  let matches = [];
+//  let matchList = [];
+
+
+// function toggleResults(action){
+// 	if (action == "show"){
+// 		nameList.classList.add("visable");
+// 	}
+// 	else {
+// 		nameList.classList.remove("visable");
+// 	}
+// }
+
+// function getMatches(inputText){
+// 	for (let i = 0 ; i < nameList.length ; i++){
+// 		if (nameList[i].toLowerCase().indexOf(inputText.toLowerCase()) != -1){
+// 			matchList.push(nameList[i])
+// 		}
+// 	}
+// 	return matchList
+// }
+
+// function displayMatches(matchList){
+// 	let j = 0 
+// 	while (j < matchList.length) {
+// 		autocompleteResults.innerHTML += "<li class=visable > " + matchList[j] + "</li>"
+// 	}
+// }
+
+//  forminput.addEventListener("keyup", (e) => {
+//  	// autocompleteResults.innerHTML = "";
+//  	toggleResults("hide");
+//  	if (forminput.value.length > 0 ){  //or can use "this" object instead of forminput
+//  		matches = getMatches(forminput.value);
+//  		if (matches.length > 0){
+//  			displayMatches(matches)
+//  		}
+
+
+//  	}
+//  })
+
+
